89f02cf2e0e64be9e4f3233a1bba382cda2bfd50
Lars <larsfinlay@googlemail.com>
2017-11-08 10:19:35 +0000

    ex shorter than 12

--- directory.rb
+++ directory.rb
@@ -23,7 +23,9 @@ end
 
 def print(students)
   students.each_with_index { |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name].length < 12
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   }
 end
 
