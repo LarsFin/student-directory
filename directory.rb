0a2259a45afbe6557039f8c2aafb94ef6bac7ded
Lars <larsfinlay@googlemail.com>
2017-11-08 09:42:50 +0000

    Hashes

--- directory.rb
+++ directory.rb
@@ -1,17 +1,17 @@
 #!/Usr/bin/env ruby
 #Let's put all student into an array
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Bowser",
-  "Ganondorf",
-  "Manus",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Bowser", cohort: :november},
+  {name: "Ganondorf", cohort: :november},
+  {name: "Manus", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 def print_header
@@ -19,9 +19,9 @@ def print_header
   puts "------------"
 end
 
-def print(names)
-  names.each { |name|
-    puts name
+def print(students)
+  students.each { |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   }
 end
 
