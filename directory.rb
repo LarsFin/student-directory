cd8125911010322199c7ca907292de777e1f23fb
Lars <larsfinlay@googlemail.com>
2017-11-08 09:59:47 +0000

    interaction

--- directory.rb
+++ directory.rb
@@ -1,18 +1,20 @@
 #!/Usr/bin/env ruby
 #Let's put all student into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Bowser", cohort: :november},
-  {name: "Ganondorf", cohort: :november},
-  {name: "Manus", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, hit enter twice"
+  #Create an empty array
+  students = []
+  #Get the first name
+  name = gets.chomp
+  #While the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  return students
+end
 
 def print_header
   puts "The students of Villains Academy"
@@ -29,6 +31,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 #Calling the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
